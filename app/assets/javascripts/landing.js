6220acdbab14b08d9ffc6e716f6f8176a98794d4
Alvarojoered <arojaspen@gmail.com>
2017-02-13 10:04:06 -0800

    Create login page

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
+
     console.log( "ready!" );
-    $(".button-collapse").sideNav();
+
+    $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+    }
+    );
+
+    $('.modal').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
 });
