4586ed31b9ae04387140d0bdca9a644ac1ae0bf0
Alvarojoered <arojaspen@gmail.com>
2017-02-16 17:58:29 -0800

    Correct JS syntax

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -42,34 +42,3 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
 }
-
-function displayModal() {
-  swal({
-    title: '<h2>Venice</h2>',
-    html:
-      `    <div class="card">
-    <div class="card-content">
-      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-    </div>
-    <div class="card-tabs">
-      <ul class="tabs tabs-fixed-width">
-        <li class="tab"><a href="#test4">Test 1</a></li>
-        <li class="tab"><a class="active" href="#test5">Test 2</a></li>
-        <li class="tab"><a href="#test6">Test 3</a></li>
-      </ul>
-    </div>
-    <div class="card-content grey lighten-4">
-      <div id="test4">Test 1</div>
-      <div id="test5">Test 2</div>
-      <div id="test6">Test 3</div>
-    </div>
-  </div>`,
-    showCloseButton: true,
-    showCancelButton: true,
-    confirmButtonText:
-      '<i class="fa fa-thumbs-up"></i> Great!',
-    cancelButtonText:
-      '<i class="fa fa-thumbs-down"></i>'
-  })
-
-}
